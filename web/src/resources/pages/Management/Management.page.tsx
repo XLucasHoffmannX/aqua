847f007c0ce421c5675b8c81beee71ecba982067
hoffmann <lucas.hoffmann@vexpenses.com>
2023-07-12 01:31:25 -0300

    MASTER adicionado uma box interativa

--- web/src/resources/pages/Management/Management.page.tsx
+++ web/src/resources/pages/Management/Management.page.tsx
@@ -1,7 +1,13 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import Wrapper from "../../../app/shared/wrapper/Wrapper";
 import { Link } from "react-router-dom";
-import { PopMarker } from "./Mangement.styles";
+import {
+  Container,
+  InteractiveBox,
+  InteractiveBoxSearch,
+  InteractiveSearch,
+  PopMarker,
+} from "./Mangement.styles";
 import { LatLngExpression } from "leaflet";
 
 export function Management(): JSX.Element {
@@ -9,26 +15,34 @@ export function Management(): JSX.Element {
 
   return (
     <Wrapper>
-      <MapContainer
-        center={{
-          lat: pointStart[0],
-          lng: pointStart[1],
-        }}
-        zoom={100}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          attribution="&copy; AquaTrack development"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={pointStart}>
-          <PopMarker>
-            Modulo 1
-            <br />
-            <Link to="#">Acessar</Link>
-          </PopMarker>
-        </Marker>
-      </MapContainer>
+      <Container>
+        <MapContainer
+          center={{
+            lat: pointStart[0],
+            lng: pointStart[1],
+          }}
+          zoom={100}
+          style={{ height: "100%", width: "100%" }}
+          zoomControl={false}
+        >
+          <TileLayer
+            attribution="&copy; AquaTrack development"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <InteractiveBox>
+            <InteractiveBoxSearch>
+              <InteractiveSearch  placeholder="Buscar por ponto..." allowClear />
+            </InteractiveBoxSearch>
+          </InteractiveBox>
+          <Marker position={pointStart}>
+            <PopMarker>
+              Modulo 1
+              <br />
+              <Link to="#">Acessar</Link>
+            </PopMarker>
+          </Marker>
+        </MapContainer>
+      </Container>
     </Wrapper>
   );
 }
--- web/src/resources/pages/Management/Mangement.styles.tsx
+++ web/src/resources/pages/Management/Mangement.styles.tsx
@@ -1,7 +1,45 @@
+import { Input, Select } from "antd";
 import { Popup } from "react-leaflet";
 import styled from "styled-components";
 
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+`;
 
 export const PopMarker = styled(Popup)`
-    border: 1px solid red;
-`
+  border: 1px solid red;
+  z-index: 9998;
+`;
+
+export const InteractiveBox = styled.div`
+  position: absolute;
+  width: 100%;
+  z-index: 8888;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`;
+
+export const InteractiveBoxSearch = styled.div`
+  margin-top: 3rem;
+  width: min(100%, 650px);
+  height: 100px;
+  background-color: var(--white);
+  border-radius: 8px;
+  box-shadow: 12px 12px 12px 0 rgba(0, 0, 0, 0.25);
+  padding: 1rem 2rem;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const InteractiveSearch = styled(Input.Search)`
+    
+`;
